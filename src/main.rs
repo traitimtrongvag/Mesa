3861f8ac69730f7b36a0c8a3373dcc48d7b393ec
traitimtrongvag <pan2512811@gmail.com>
2026-02-10 06:02:57 +0000

    chore: update module declarations in main entry point

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+mod protocol;
 mod server;
+mod token;
 
 #[tokio::main]
 async fn main() {
-    // Lấy port từ biến môi trường (Render)
     let port: u16 = std::env::var("PORT")
-        .unwrap_or("8081".to_string())
+        .unwrap_or_else(|_| "8081".to_string())
         .parse()
         .unwrap_or(8081);
 
